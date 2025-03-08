c89195c301a2a77a3751286df0a663e6cb5ba6bb
Jonathan Oliveira <jonathanleiteoliveira@hotmail.com>
2025-03-08 20:37:52 +0000

    Atualização do Python

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload TESTE 2.
+          Edit <code>src/App.js</code> and save to reload TESTE T.
         </p>
         <a
           className="App-link"
